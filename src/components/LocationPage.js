c04b9e64ff82c6d65f1744fb481350092548cf89
shenjiajun <shenjj@2345.com>
2017-01-18 18:12:09 +0800

    优化

--- src/components/LocationPage.js
+++ src/components/LocationPage.js
@@ -15,16 +15,23 @@ import {
     ScrollView,
     Platform,
     BackAndroid,
-    Image
+    Image,
+    Dimensions
 } from 'react-native';
-import {findCityByName} from  '../Utils.js'
+import {findCityByName, findCityByGeoLocation} from  '../Utils.js'
 
 class TitleBar extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            textValue: ""
+        }
     }
 
     handleChange(value) {
+        this.setState({
+            textValue: value
+        });
         let url = findCityByName(value);
         // console.log("input = " + value + " \n url=" + url);
         fetch(url)
@@ -46,15 +53,35 @@ class TitleBar extends React.Component {
             });
     }
 
+    clearText() {
+        this.setState({
+            textValue: ""
+        });
+        this.props.onResponseLocation(null);
+    }
+
     render() {
         return (
-            <View style={{}}>
-                <TextInput
-                    style={{width: 300}}
-                    onChangeText={(value) => this.handleChange(value)}
-                    placeholder="输入城市名"
-                >
-                </TextInput>
+            <View>
+                <View style={{flexDirection: "row", alignItems: "center", backgroundColor: "#ffffff"}}>
+                    <TouchableOpacity onPress={() => this.props.onBackClick()}>
+                        <Image source={require("../../res/drawable-xhdpi/back_arrow.png")}
+                               style={{height: 30, width: 30, marginRight: 5, marginLeft: 16}}/>
+                    </TouchableOpacity>
+                    <TextInput
+                        value={this.state.textValue}
+                        style={{width: 300, flex: 1}}
+                        onChangeText={(value) => this.handleChange(value)}
+                        placeholder="输入城市名"
+                        underlineColorAndroid={"#00000000"}
+                    >
+                    </TextInput>
+                    <TouchableOpacity onPress={() => this.clearText()}>
+                        <Image source={require("../../res/drawable-xhdpi/text_clear.png")}
+                               style={{height: 30, width: 30, marginRight: 16, marginLeft: 5}}/>
+                    </TouchableOpacity>
+                </View>
+                <View style={{backgroundColor: "#ebebeb", height: 1, alignItems: "stretch"}}/>
             </View>
         );
     }
@@ -115,7 +142,7 @@ class AddressList extends React.Component {
                 </View>
             );
         } else {
-            return (<View></View>);
+            return (<View/>);
         }
     }
 }
@@ -149,6 +176,39 @@ class LocationPage extends React.Component {
         }
     }
 
+    getLocation() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.getCityByLocation(position);
+            },
+            (error) => alert(error.message),
+            {enableHighAccuracy: true, timeout: 5000, maximumAge: 1000})
+    }
+
+    getCityByLocation(position) {
+        // const positionStr = JSON.stringify(position);
+        // console.log("position=" + positionStr);
+        let url = findCityByGeoLocation(position.coords.latitude + "," + position.coords.longitude);
+        console.log("url=" + url);
+        fetch(url)
+            .then(
+                (response) => response.json()
+            ).then(
+            (json) => {
+                console.log(JSON.stringify(json));
+                let mCityBean = json;
+                if (mCityBean.metadata.status_code == 200) {
+                    this.onResponseLocation(json);
+                } else {
+                    this.onResponseLocation(null);
+                }
+            }
+        ).catch(
+            (ex) => {
+                console.warn('parsing failed', ex);
+            });
+    }
+
     onResponseLocation(value) {
         // console.log("responseLocation=" + value);
         this.setState(
@@ -168,11 +228,10 @@ class LocationPage extends React.Component {
             <View style={{
                 flex: 1,
                 flexDirection: "column",
-                top: 0,
-                left: 0,
                 backgroundColor: "#ffffff"
             }}>
-                <TitleBar onResponseLocation={(value) => this.onResponseLocation(value)}/>
+                <TitleBar onResponseLocation={(value) => this.onResponseLocation(value)}
+                          onBackClick={() => this.props.onBack()}/>
 
                 <AddressList style={{
                     flexWrap: "wrap",
@@ -181,6 +240,26 @@ class LocationPage extends React.Component {
                              addressResponse={this.state.addressResponse}
                              getSelectedLocation={(value) => this.getSelectedLocation(value)}>
                 </AddressList>
+
+
+                <View style={{
+                    borderRadius: 50,
+                    backgroundColor: "#FF4081",
+                    padding: 10,
+                    position: "absolute",
+                    left: Dimensions.get('window').width - 60,
+                    top: Dimensions.get('window').height - 100,
+                }}>
+                    <TouchableOpacity onPress={() => this.getLocation()}>
+                        <Image
+                            style={{
+                                height: 30,
+                                width: 30,
+                            }}
+                            source={require("../../res/drawable-xhdpi/location_bg.png")}
+                        />
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
